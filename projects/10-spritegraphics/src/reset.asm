dd999123b5928d0f93643dc8e7755ea8609a1a27
Kevin Zurawel <kevinz@famicom.party>
2019-08-16 10:18:59 -0500

    finish Chapter 10

--- projects/10-spritegraphics/src/reset.asm
+++ projects/10-spritegraphics/src/reset.asm
@@ -13,14 +13,5 @@ vblankwait:
   BIT PPUSTATUS
   BPL vblankwait
 
-  LDA #$70
-  STA $0200
-  LDA #$05
-  STA $0201
-  LDA #$00
-  STA $0202
-  LDA #$80
-  STA $0203
-
   JMP main
 .endproc
--- projects/10-spritegraphics/src/sprites.asm
+++ projects/10-spritegraphics/src/sprites.asm
@@ -18,6 +18,7 @@
 
 .export main
 .proc main
+  ; write a palette
   LDX PPUSTATUS
   LDX #$3f
   STX PPUADDR
@@ -25,8 +26,32 @@
   STX PPUADDR
   LDA #$29
   STA PPUDATA
-  LDA #%00011110
+  LDA #$19
+  STA PPUDATA
+  LDA #$09
+  STA PPUDATA
+  LDA #$0f
+  STA PPUDATA
+
+  ; write sprite data
+  LDA #$70
+  STA $0200
+  LDA #$05
+  STA $0201
+  LDA #$00
+  STA $0202
+  LDA #$80
+  STA $0203
+
+vblankwait:       ; wait for another vblank before continuing
+  BIT PPUSTATUS
+  BPL vblankwait
+
+  LDA #%10010000  ; turn on NMIs, sprites use first pattern table
+  STA PPUCTRL
+  LDA #%00011110  ; turn on screen
   STA PPUMASK
+
 forever:
   JMP forever
 .endproc
@@ -35,4 +60,4 @@ forever:
 .addr nmi_handler, reset_handler, irq_handler
 
 .segment "CHR"
-.res 8192
+.incbin "graphics.chr"
